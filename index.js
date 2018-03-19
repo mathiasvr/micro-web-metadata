d351e6f6207f22e5d89da8966a762071e629f1e9
Mathias Rasmussen <mathiasvr@gmail.com>
2018-03-19 02:14:47 +0100

    update dependencies

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { parse } = require('url')
-const { scrapeUrl } = require('metascraper')
+const metascraper = require('metascraper')
+const got = require('got')
 const { handleErrors, createError } = require('micro-boom')
 
 module.exports = handleErrors(async (req, res) => {
@@ -10,7 +11,8 @@ module.exports = handleErrors(async (req, res) => {
   }
 
   try {
-    return await scrapeUrl(url)
+    const {body: html, url: responseUrl} = await got(url)
+    return await metascraper({html, url: responseUrl})
   } catch (err) {
     console.error(`${req.url}: ${err.message}`)
     throw createError(400, err)
